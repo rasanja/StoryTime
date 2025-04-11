0adbe0d7b448f055d7b4236a72e6097a4253eb67
Rasanja Dampriya <rasanjadampriya@hotmail.com>
2025-04-11 04:05:22 -0400

    feat: add permission handling for camera, storage and internet
    implemented runtime permission checks based on android version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //<--- Camera Dependencies --->
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation(libs.androidx.camera.lifecycle)
+
+    // If you want to additionally use the CameraX View class
+    implementation(libs.androidx.camera.view)
 }
--- app/src/main/java/cs477/gmu/project3_rdelphec/MainActivity.kt
+++ app/src/main/java/cs477/gmu/project3_rdelphec/MainActivity.kt
@@ -1,8 +1,14 @@
 package cs477.gmu.project3_rdelphec
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Toast
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,6 +17,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import cs477.gmu.project3_rdelphec.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -38,6 +47,12 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        if(!hasRequiredPermissions()){ //check for permissions
+            ActivityCompat.requestPermissions(
+                this, requiredPermissions, 0
+            )
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -50,4 +65,72 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+
+    //========================================
+    //         PERMISSION HANDLING
+    //========================================
+
+    /**
+     * Checks if app has the correct permissions to function.
+     * @return : true if all permissions are provided, false if at least one is not provided.
+     */
+    private fun hasRequiredPermissions(): Boolean{
+        return requiredPermissions.all { permission-> //loop through each permission.
+            ContextCompat.checkSelfPermission(
+                this, permission
+            ) == PackageManager.PERMISSION_GRANTED //returns false if even 1 permission is not provided.
+        }
+    }
+
+    /**
+     * An array of permissions needed for the app to function.
+     * On runtime checks the SDK version of device and determines which permissions to request.
+     */
+    private val requiredPermissions: Array<String>
+        get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){ //if newer than Tiramisu don't ask for READ_EXTERNAL
+            arrayOf(
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.CAMERA
+            )
+        }else{ //if older ask for READ_EXTERNAL
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+        }
+
+    /**
+     * Handles the result of request permission.
+     * Toasts a message accordingly.
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }){
+            Toast.makeText(this, "All permissions granted", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "Permission denied. App cannot work properly", Toast.LENGTH_SHORT).show()
+        }
+
+        if(!isInternetAvailable()){
+            Toast.makeText(this, "No network connection", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /**
+     *  Checks if the app has access to a working internet connection
+     *  @return true if internet works, false if not
+     */
+    private fun isInternetAvailable():Boolean{
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
 }
